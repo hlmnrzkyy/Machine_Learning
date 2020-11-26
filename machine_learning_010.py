a6e435cb099b3c995a90df3fa4097c910ef2cdde
Hilman Rizky Yudhaperwira <74447947+hlmnrzkyy@users.noreply.github.com>
2020-11-26 18:35:56 +0700

    Update machine_learning_010.py

--- machine_learning_010.py
+++ machine_learning_010.py
@@ -1,9 +1,13 @@
 import tensorflow as tf
-from tensorflow.keras.optimizers import RMSprop
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import zipfile
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+from keras.preprocessing import image
+from PIL import Image
 
 # melakukan ekstraksi pada file zip
-import zipfile,os
 local_zip = 'messy-vs-clean-room.zip'
 zip_ref = zipfile.ZipFile(local_zip, 'r')
 zip_ref.extractall('/tmp')
@@ -37,15 +41,15 @@ train_datagen = ImageDataGenerator(
                     rescale=1./255,
                     rotation_range=20,
                     horizontal_flip=True,
-                    shear_range = 0.2,
-                    fill_mode = 'nearest')
+                    shear_range=0.2,
+                    fill_mode='nearest')
 
 test_datagen = ImageDataGenerator(
                     rescale=1./255,
                     rotation_range=20,
                     horizontal_flip=True,
-                    shear_range = 0.2,
-                    fill_mode = 'nearest')
+                    shear_range=0.2,
+                    fill_mode='nearest')
 
 train_generator = train_datagen.flow_from_directory(
         train_dir,  # direktori data latih
@@ -55,20 +59,20 @@ train_generator = train_datagen.flow_from_directory(
         class_mode='binary')
 
 validation_generator = test_datagen.flow_from_directory(
-        validation_dir, # direktori data validasi
-        target_size=(150, 150), # mengubah resolusi seluruh gambar menjadi 150x150 piksel
-        batch_size=4, # karena ini merupakan masalah klasifikasi 2 kelas maka menggunakan class_mode = 'binary'
+        validation_dir,  # direktori data validasi
+        target_size=(150, 150),  # mengubah resolusi seluruh gambar menjadi 150x150 piksel
+        batch_size=4,  # karena ini merupakan masalah klasifikasi 2 kelas maka menggunakan class_mode = 'binary'
         class_mode='binary')
 
 model = tf.keras.models.Sequential([
-    tf.keras.layers.Conv2D(32, (3,3), activation='relu', input_shape=(150, 150, 3)),
+    tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(150, 150, 3)),
     tf.keras.layers.MaxPooling2D(2, 2),
-    tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
-    tf.keras.layers.MaxPooling2D(2,2),
-    tf.keras.layers.Conv2D(128, (3,3), activation='relu'),
-    tf.keras.layers.MaxPooling2D(2,2),
-    tf.keras.layers.Conv2D(512, (3,3), activation='relu'),
-    tf.keras.layers.MaxPooling2D(2,2),
+    tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    tf.keras.layers.Conv2D(128, (3, 3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    tf.keras.layers.Conv2D(512, (3, 3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2, 2),
     tf.keras.layers.Flatten(),
     tf.keras.layers.Dense(128, activation='relu'),
     tf.keras.layers.Dense(1, activation='sigmoid')
@@ -82,15 +86,11 @@ history = model.fit(
       train_generator,
       steps_per_epoch=25,  # berapa batch yang akan dieksekusi pada setiap epoch
       epochs=25,
-      validation_data=validation_generator, # menampilkan akurasi pengujian data validasi
+      validation_data=validation_generator,  # menampilkan akurasi pengujian data validasi
       validation_steps=5,  # berapa batch yang akan dieksekusi pada setiap epoch
       verbose=2)
 
 # Prediksi Gambar
-import numpy as np
-import matplotlib.pyplot as plt
-from keras.preprocessing import image
-from PIL import Image
 
 filename = ["0.png", "1.png", "3.png",
             "4.png", "5.png", "6.png",
